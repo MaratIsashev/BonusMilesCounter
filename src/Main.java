349119e012771d2c955c79f9020d4f80bff81909
Marat Isashev <isashevmarat@gmail.com>
2023-01-31 09:45:48 +0300

    class added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 14_000;
-        int oneBonusMile = 20;
+        // int ticketPrice = 14_000;
+        // int oneBonusMile = 20;
 
-        System.out.println("За ваш билет будет начислено " + (ticketPrice / oneBonusMile) + " бонусных миль");
+        // System.out.println("За ваш билет будет начислено " + (ticketPrice / oneBonusMile) + " бонусных миль");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
